07569b621712aa78ecf9f7f6db83956d78ae738f
kimplausible127 <kimplausible127@gmail.com>
2022-06-27 16:15:13 -0700

    added canonical url for Post model, or get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from .models import Post
@@ -50,3 +51,10 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
+    
